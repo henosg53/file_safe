532b1b2f5e000062d7195a4abd92980a9dbb9b11
henosg53 <henosg53@gmail.com>
2023-12-26 18:39:25 -0500

    first meaningful commit

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-def crypt_ui():
-    from xcrypt_ui import XCryptUI, XAuth, XConfig
-    # app = XCryptUI()
-    XAuth()
-    # XConfig()
-
-
-def ui_training():
-    from ui_training import TrainingUI, Other
-    # app = TrainingUI()
-    app = Other()
+from xcrypt_ui import XAuth
 
 
 if __name__ == '__main__':
-    crypt_ui()
-    # ui_training()
+    XAuth()
